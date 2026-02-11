b3ef0ca3a73151747443051104ca68a52c481094
Raza Ali <alic5146@gmail.com>
2026-02-11 22:47:31 +0500

    Enable SSL for Railway PostgreSQL

--- db.js
+++ db.js
@@ -3,9 +3,13 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
+
