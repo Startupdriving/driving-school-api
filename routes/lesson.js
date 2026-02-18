a9610163a38c1f3923c322f7e20c5865c746f723
Raza Ali <alic5146@gmail.com>
2026-02-18 21:19:10 +0500

    Add reschedule lesson endpoint

--- routes/lesson.js
+++ routes/lesson.js
@@ -2,14 +2,20 @@ import express from "express";
 import {
   scheduleLesson,
   cancelLesson,
-  completeLesson
+  completeLesson,
+  rescheduleLesson
 } from "../services/lessonService.js";
+
 
 const router = express.Router();
 
 router.post("/schedule", scheduleLesson);
 router.post("/cancel", cancelLesson);
 router.post("/complete", completeLesson);
+router.post("/schedule", scheduleLesson);
+router.post("/cancel", cancelLesson);
+router.post("/complete", completeLesson);
+router.post("/reschedule", rescheduleLesson);
 
 export default router;
 
