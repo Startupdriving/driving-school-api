2d45e394d250d7b8562a37dc7c2a26b3d04490fa
Raza Ali <alic5146@gmail.com>
2026-02-21 09:45:13 +0500

    FIX: lesson request

--- routes/lessonRequest.js
+++ routes/lessonRequest.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { requestLesson, sendOffer, acceptOffer } from "../services/lessonRequestService.js";
+import { requestLesson, acceptOffer } from "../services/lessonRequestService.js";
 
 const router = express.Router();
 
 router.post("/request", requestLesson);
-router.post("/send-offer", sendOffer);
 router.post("/accept-offer", acceptOffer);
 
 export default router;
