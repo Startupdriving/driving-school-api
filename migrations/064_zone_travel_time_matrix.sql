a454804b9127d39db83d0991d986bbbb83d34857
Raza Ali <alic5146@gmail.com>
2026-03-05 19:12:37 +0500

    fix travel matrix seed avg_travel_minutes

--- migrations/064_zone_travel_time_matrix.sql
+++ migrations/064_zone_travel_time_matrix.sql
@@ -11,17 +11,22 @@ CREATE TABLE IF NOT EXISTS zone_travel_time_matrix (
 ALTER TABLE zone_travel_time_matrix
 ADD COLUMN IF NOT EXISTS estimated_minutes NUMERIC(5,2);
 
--- seed travel times
 INSERT INTO zone_travel_time_matrix
-(from_zone_id, to_zone_id, estimated_minutes)
+(from_zone_id, to_zone_id, avg_travel_minutes, estimated_minutes)
 SELECT
   a.id,
   b.id,
   CASE
     WHEN a.id = b.id THEN 5
     ELSE 20
+  END,
+  CASE
+    WHEN a.id = b.id THEN 5
+    ELSE 20
   END
 FROM geo_zones a
 CROSS JOIN geo_zones b
-ON CONFLICT (from_zone_id, to_zone_id) DO UPDATE
-SET estimated_minutes = EXCLUDED.estimated_minutes;
+ON CONFLICT (from_zone_id, to_zone_id)
+DO UPDATE SET
+  avg_travel_minutes = EXCLUDED.avg_travel_minutes,
+  estimated_minutes = EXCLUDED.estimated_minutes;
